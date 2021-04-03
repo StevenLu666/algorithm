689e1b6d773e2fedd20bde54b05b234c0f97ca69
Steven Lu <bingoau2015@gmail.com>
2021-04-03 12:16:45 +1100

    resolve leetcode 15

--- Leetcode/editor/en/_15_ThreeSum.java
+++ Leetcode/editor/en/_15_ThreeSum.java
@@ -6,13 +6,14 @@ public class _15_ThreeSum{
     
     public void run() {
         Solution s = new Solution();
-        int []a = new int[] {-1,0,1,2,-1,-4};
-        //int []a = new int[] {};
+//        int []a = new int[] {-1,0,1,2,-1,-4};
+//        int []a = new int[] {};
+        int []a = new int[] {-2,0,0,2,2};
         List<List<Integer>> result = s.threeSum(a);
         System.out.println(result);
     }
-
-    //leetcode submit region begin(Prohibit modification and deletion)
+    /*
+    // first round.
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> result = new LinkedList<>();
@@ -43,6 +44,41 @@ class Solution {
             while(i < nums.length-3 && nums[i] == nums[i+1])
                 i++;
         }
+        return result;
+    }
+}*/
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new LinkedList<>();
+        Arrays.sort(nums);
+
+        for(int i = 0; i < nums.length-2; i++) {
+            if (i > 0 && nums[i] == nums[i-1])
+                continue;
+            int j = i+1, k = nums.length-1;
+            int sum = -nums[i];
+            int tmp;
+            while (j<k) {
+                tmp = nums[j] + nums[k];
+                if (tmp == sum) {
+                    List<Integer> r = Arrays.asList(nums[i], nums[j], nums[k]);
+                    ((LinkedList<List<Integer>>) result).push(r);
+                    j++;
+                    k--;
+                    while(j<k && nums[j] == nums[j-1])  j++;
+                    while(j<k && nums[k] == nums[k+1])  k--;
+                } else if (tmp < sum) {
+                    j++;
+                    while(j<k && nums[j] == nums[j-1])  j++;
+                } else {
+                    k--;
+                    while(j<k && nums[k] == nums[k+1])  k--;
+                }
+            }
+        }
+
         return result;
     }
 }
