ad00426560965c8dc77ed16f50ce71443278c7ce
Steven Lu <bingoau2015@gmail.com>
2021-04-05 17:33:52 +1000

    resolve leetcode 21

--- Leetcode/editor/en/_21_MergeTwoSortedLists.java
+++ Leetcode/editor/en/_21_MergeTwoSortedLists.java
@@ -36,7 +36,7 @@ public class _21_MergeTwoSortedLists{
         cc.next = ff;
         ff.next = gg;
 
-        ListNode r = s.mergeTwoLists(a, aa);
+        ListNode r = s.mergeTwoLists(f, ff);
         print(r);
 
     }
@@ -73,29 +73,42 @@ class Solution {
             return l2;
         if (l2 == null)
             return l1;
-        ListNode ret = new ListNode(0);
-        ListNode tail = new ListNode();
-        while (l1 != null && l2 != null) {
-            if (l1.val < l2.val) {
-                if (ret.next == null)
-                    ret.next = l1;
-                tail.next = l1;
-                tail = l1;
-                l1 = l1.next;
-            } else {
-                if (ret.next == null)
-                    ret.next = l2;
-                tail.next = l2;
-                tail = l2;
-                l2 = l2.next;
-            }
+        if (l1.val < l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
         }
-        if (l1 == null)
-            tail.next = l2;
-        else
-            tail.next = l1;
-        return ret.next;
     }
+//    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+//        if (l1 == null)
+//            return l2;
+//        if (l2 == null)
+//            return l1;
+//        ListNode ret = new ListNode(0);
+//        ListNode tail = new ListNode();
+//        while (l1 != null && l2 != null) {
+//            if (l1.val < l2.val) {
+//                if (ret.next == null)
+//                    ret.next = l1;
+//                tail.next = l1;
+//                tail = l1;
+//                l1 = l1.next;
+//            } else {
+//                if (ret.next == null)
+//                    ret.next = l2;
+//                tail.next = l2;
+//                tail = l2;
+//                l2 = l2.next;
+//            }
+//        }
+//        if (l1 == null)
+//            tail.next = l2;
+//        else
+//            tail.next = l1;
+//        return ret.next;
+//    }
 }
 //leetcode submit region end(Prohibit modification and deletion)
 
