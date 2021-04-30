f439161a2aae9049eb218b0753c0485829f2499e
Steven Lu <bingoau2015@gmail.com>
2021-04-30 12:56:01 +1000

    another solution for 46

--- Leetcode/editor/en/_46_Permutations.java
+++ Leetcode/editor/en/_46_Permutations.java
@@ -13,7 +13,7 @@ public class _46_Permutations{
         System.out.println(Arrays.toString(result.toArray()));
     }
 
-    //leetcode submit region begin(Prohibit modification and deletion)
+    /*
 class Solution {
 
     private void helper(List<Integer> remainList, int index, List<Integer> outputList, List<List<Integer>> result) {
@@ -43,8 +43,37 @@ class Solution {
         }
         return result;
     }
-}
-//leetcode submit region end(Prohibit modification and deletion)
+    */
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public List<List<Integer>> permute(int[] nums) {
+            List<List<Integer>> result = new ArrayList<>();
+            List<Integer> list = new ArrayList<>();
+            boolean []used = new boolean[nums.length];
+            Arrays.fill(used, false);
+            Arrays.sort(nums);
+            helper(result, list, nums, used);
+            return result;
+        }
 
+        private void helper(List<List<Integer>> result, List<Integer> list, int[] nums, boolean[] used) {
+            if (list.size() == nums.length) {
+                result.add(new ArrayList<>(list));
+                return;
+            }
+
+            for (int i = 0; i < nums.length; i++) {
+                if (used[i])
+                    continue;
+                used[i] = true;
+                list.add(nums[i]);
+                helper(result, list, nums, used);
+                list.remove(list.size()-1);
+                used[i] = false;
+            }
+
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
 
 }
