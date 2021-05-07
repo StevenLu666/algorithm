4bcdd0e218eb265edd5a271f8c1ba3db5495e96f
Steven Lu <bingoau2015@gmail.com>
2021-05-07 22:16:45 +1000

    solution2 for 169

--- Leetcode/editor/en/_169_MajorityElement.java
+++ Leetcode/editor/en/_169_MajorityElement.java
@@ -1,5 +1,6 @@
 package editor.en;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -10,26 +11,32 @@ public class _169_MajorityElement{
         int r = s.majorityElement(new int[] {2,2,1,1,1,2,2});
         System.out.println(r);
     }
+    class Solution1 {
+        public int majorityElement(int[] nums) {
+            if (nums.length == 1)
+                return nums[0];
+            Map<Integer, Integer> set = new HashMap<>();
+            for(int num: nums) {
+                if (!set.containsKey(num)) {
+                    set.put(num, 1);
+                } else {
+                    int value = set.get(num)+1;
+                    if (value > nums.length/2)
+                        return num;
+                    set.put(num, value);
+                }
+            }
+            return 0;
+        }
+    }
 
     //leetcode submit region begin(Prohibit modification and deletion)
-class Solution {
-    public int majorityElement(int[] nums) {
-        if (nums.length == 1)
-            return nums[0];
-        Map<Integer, Integer> set = new HashMap<>();
-        for(int num: nums) {
-            if (!set.containsKey(num)) {
-                set.put(num, 1);
-            } else {
-                int value = set.get(num)+1;
-                if (value > nums.length/2)
-                    return num;
-                set.put(num, value);
-            }
+    class Solution {
+        public int majorityElement(int[] nums) {
+            Arrays.sort(nums);
+            return nums[nums.length/2];
         }
-        return 0;
     }
-}
 //leetcode submit region end(Prohibit modification and deletion)
 
 
