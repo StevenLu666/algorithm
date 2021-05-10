a572077af5636b4c2ac7debc5a23740f2753b032
Steven Lu <bingoau2015@gmail.com>
2021-05-10 23:50:35 +1000

    169. divide and conquer

--- Leetcode/editor/en/_169_MajorityElement.java
+++ Leetcode/editor/en/_169_MajorityElement.java
@@ -8,9 +8,11 @@ public class _169_MajorityElement{
     
     public void run() {
         Solution s = new Solution();
-        int r = s.majorityElement(new int[] {2,2,1,1,1,2,2});
+        int r = s.majorityElement(new int[] {8,9,8,9,8});
         System.out.println(r);
     }
+
+    // Hash table
     class Solution1 {
         public int majorityElement(int[] nums) {
             if (nums.length == 1)
@@ -29,6 +31,8 @@ public class _169_MajorityElement{
             return 0;
         }
     }
+
+    // sort
     class Solution2 {
         public int majorityElement(int[] nums) {
             Arrays.sort(nums);
@@ -36,9 +40,8 @@ public class _169_MajorityElement{
         }
     }
 
-    //leetcode submit region begin(Prohibit modification and deletion)
     // same number will increase count, and different number will decrease count.
-    class Solution {
+    class Solution3 {
         public int majorityElement(int[] nums) {
             int major = nums[0];
             int count = 0;
@@ -54,6 +57,33 @@ public class _169_MajorityElement{
             }
             return major;
         }
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    // Divide and conquer
+    class Solution {
+        public int majorityElement(int[] nums) {
+            return majority(nums, 0, nums.length-1);
+        }
+
+        private int majority(int[] nums, int l, int r) {
+            if (l == r)
+                return nums[l];
+            int m = (l+r)/2;
+            int lm = majority(nums, l, m);
+            int mr = majority(nums, m+1, r);
+            if (lm == mr)
+                return lm;
+            int countLm = 0, countMr = 0;
+            for (int i = l; i <= r; i++) {
+                if (nums[i] == lm)
+                    countLm++;
+                else if (nums[i] == mr)
+                    countMr++;
+            }
+            return countLm > countMr ? lm : mr;
+        }
+
     }
 //leetcode submit region end(Prohibit modification and deletion)
 
